80146c20614fcb928c80f250b84a85284c7ecae0
Harald Ponce de Leon <hpdl@oscommerce.com>
2006-07-18 17:12:57 +0000

    merge hpdl r643
    key() accepts its parameter as a reference only
    
    fix #48

--- account.php
+++ account.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2005 osCommerce
+  Copyright (c) 2006 osCommerce
 
   Released under the GNU General Public License
 */
@@ -13,7 +13,11 @@
   require('includes/application_top.php');
 
   if ($osC_Customer->isLoggedOn() === false) {
-    if (empty($_GET) || (!empty($_GET) && !in_array(tep_sanitize_string(basename(key(array_slice($_GET, 0, 1)))), array('login', 'create', 'password_forgotten')))) {
+    if (!empty($_GET)) {
+      $first_array = array_slice($_GET, 0, 1);
+    }
+
+    if (empty($_GET) || (!empty($_GET) && !in_array(tep_sanitize_string(basename(key($first_array))), array('login', 'create', 'password_forgotten')))) {
       $osC_NavigationHistory->setSnapshot();
 
       tep_redirect(tep_href_link(FILENAME_ACCOUNT, 'login', 'SSL'));
