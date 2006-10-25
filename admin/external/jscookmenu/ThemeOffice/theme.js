2fb0861344751a504aa605edc0888012ae53eaf7
Harald Ponce de Leon <hpdl@oscommerce.com>
2006-10-25 15:38:49 +0000

    merge from hpdl r1069; update jscookmenu to the latest version

--- admin/external/jscookmenu/ThemeOffice/theme.js
+++ admin/external/jscookmenu/ThemeOffice/theme.js
@@ -17,6 +17,7 @@ catch (e)
 
 var cmThemeOffice =
 {
+	prefix:	'ThemeOffice',
   	// main menu display attributes
   	//
   	// Note.  When the menu bar is horizontal,
@@ -36,23 +37,33 @@ var cmThemeOffice =
 
 	// sub menu display attributes
 
-	// 0, HTML code to the left of the folder item
+	// HTML code to the left of the folder item
 	folderLeft: '<img alt="" src="' + cmThemeOfficeBase + 'spacer.gif">',
-	// 1, HTML code to the right of the folder item
+	// HTML code to the right of the folder item
 	folderRight: '<img alt="" src="' + cmThemeOfficeBase + 'arrow.gif">',
-	// 2, HTML code to the left of the regular item
+	// HTML code to the left of the regular item
 	itemLeft: '<img alt="" src="' + cmThemeOfficeBase + 'spacer.gif">',
-	// 3, HTML code to the right of the regular item
+	// HTML code to the right of the regular item
 	itemRight: '<img alt="" src="' + cmThemeOfficeBase + 'blank.gif">',
-	// 4, cell spacing for main menu
+	// cell spacing for main menu
 	mainSpacing: 0,
-	// 5, cell spacing for sub menus
+	// cell spacing for sub menus
 	subSpacing: 0,
-	// 6, auto dispear time for submenus in milli-seconds
-	delay: 500
+
+	subMenuHeader: '<div class="ThemeOfficeSubMenuShadow"></div>',
+
+	offsetHMainAdjust:	[-1, 0],
+	offsetSubAdjust:	[-1, -1]
+
+	// rest use default settings
 };
 
 // for horizontal menu split
-var cmThemeOfficeHSplit = [_cmNoClick, '<td class="ThemeOfficeMenuItemLeft"></td><td colspan="2"><div class="ThemeOfficeMenuSplit"></div></td>'];
-var cmThemeOfficeMainHSplit = [_cmNoClick, '<td class="ThemeOfficeMainItemLeft"></td><td colspan="2"><div class="ThemeOfficeMenuSplit"></div></td>'];
+var cmThemeOfficeHSplit = [_cmNoClick, '<td class="ThemeOfficeMenuItemLeft"></td><td colspan="2" class="ThemeOfficeMenuSplit"><div class="ThemeOfficeMenuSplit"></div></td>'];
+var cmThemeOfficeMainHSplit = [_cmNoClick, '<td class="ThemeOfficeMainItemLeft"></td><td colspan="2" class="ThemeOfficeMenuSplit"><div class="ThemeOfficeMenuSplit"></div></td>'];
 var cmThemeOfficeMainVSplit = [_cmNoClick, '|'];
+
+/* IE can't do negative margin on tables */
+/*@cc_on
+	cmThemeOffice.subMenuHeader = '<div class="ThemeOfficeSubMenuShadow" style="background-color: #cccccc; filter: alpha(opacity=35);"></div>';
+@*/
