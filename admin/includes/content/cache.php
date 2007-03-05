9ef2c9d053ebbc964deda5489b1fcc88eddba29b
Harald Ponce de Leon <hpdl@oscommerce.com>
2007-03-05 22:40:44 +0000

    merge from hpdl r1278
    move processing logic to the class level 
    separate presentation into multiple pages, for each action available

--- admin/includes/content/cache.php
+++ admin/includes/content/cache.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2006 osCommerce
+  Copyright (c) 2007 osCommerce
 
   Released under the GNU General Public License
 */
@@ -16,43 +16,55 @@
 
     var $_module = 'cache',
         $_page_title = HEADING_TITLE,
-        $_page_contents = 'cache.php';
+        $_page_contents = 'main.php';
 
 /* Class constructor */
 
     function osC_Content_Cache() {
       global $osC_MessageStack;
 
-      if (!isset($_GET['action'])) {
+      if ( !isset($_GET['action']) ) {
         $_GET['action'] = '';
       }
 
 // check if the cache directory exists
-      if (is_dir(DIR_FS_WORK)) {
-        if (!is_writeable(DIR_FS_WORK)) {
+      if ( is_dir(DIR_FS_WORK) ) {
+        if ( !is_writeable(DIR_FS_WORK) ) {
           $osC_MessageStack->add('header', ERROR_CACHE_DIRECTORY_NOT_WRITEABLE, 'error');
         }
       } else {
         $osC_MessageStack->add('header', ERROR_CACHE_DIRECTORY_DOES_NOT_EXIST, 'error');
       }
 
-      if (!empty($_GET['action'])) {
-        switch ($_GET['action']) {
-          case 'reset':
-            $this->_reset();
+      if ( !empty($_GET['action']) ) {
+        switch ( $_GET['action'] ) {
+          case 'delete':
+/*HPDL
+            if ( osC_Cache::clear($_GET['block']) ) {
+              $osC_MessageStack->add_session($this->_module, SUCCESS_DB_ROWS_UPDATED, 'success');
+            } else {
+              $osC_MessageStack->add_session($this->_module, ERROR_DB_ROWS_NOT_UPDATED, 'error');
+            }
+*/
+
+            osC_Cache::clear($_GET['block']);
+
+            osc_redirect(osc_href_link_admin(FILENAME_DEFAULT, $this->_module));
+
+            break;
+
+          case 'batchDelete':
+            if ( isset($_POST['batch']) && is_array($_POST['batch']) && !empty($_POST['batch']) ) {
+              foreach ($_POST['batch'] as $id) {
+                osC_Cache::clear($id);
+              }
+
+              osc_redirect(osc_href_link_admin(FILENAME_DEFAULT, $this->_module));
+            }
+
             break;
         }
       }
-    }
-
-/* Private methods */
-
-    function _reset() {
-      if (isset($_GET[$this->_module]) && !empty($_GET[$this->_module])) {
-        osC_Cache::clear($_GET[$this->_module]);
-      }
-
-      osc_redirect(osc_href_link_admin(FILENAME_DEFAULT, $this->_module));
     }
   }
 ?>
---
+++ admin/templates/pages/cache/main.php
@@ -0,0 +1,96 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2007 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  $osC_DirectoryListing = new osC_DirectoryListing(DIR_FS_WORK);
+  $osC_DirectoryListing->setIncludeDirectories(false);
+  $osC_DirectoryListing->setCheckExtension('cache');
+
+  $cached_files = array();
+
+  foreach ( $osC_DirectoryListing->getFiles() as $file ) {
+    $last_modified = filemtime(DIR_FS_WORK . '/' . $file['name']);
+
+    if ( strpos($file['name'], '-') !== false ) {
+      $code = substr($file['name'], 0, strpos($file['name'], '-'));
+    } else {
+      $code = substr($file['name'], 0, strpos($file['name'], '.'));
+    }
+
+    if ( isset($cached_files[$code]) ) {
+      $cached_files[$code]['total']++;
+
+      if ( $last_modified > $cached_files[$code]['last_modified'] ) {
+        $cached_files[$code]['last_modified'] = $last_modified;
+      }
+    } else {
+      $cached_files[$code] = array('total' => 1,
+                                   'last_modified' => $last_modified);
+    }
+  }
+?>
+
+<h1><?php echo osc_link_object(osc_href_link(FILENAME_DEFAULT, $osC_Template->getModule()), $osC_Template->getPageTitle()); ?></h1>
+
+<?php
+  if ( $osC_MessageStack->size($osC_Template->getModule()) > 0 ) {
+    echo $osC_MessageStack->output($osC_Template->getModule());
+  }
+?>
+
+<form name="batch" action="#" method="post">
+
+<table border="0" width="100%" cellspacing="0" cellpadding="2" class="dataTable">
+  <thead>
+    <tr>
+      <th><?php echo TABLE_HEADING_CACHE; ?></th>
+      <th><?php echo TABLE_HEADING_TOTAL; ?></th>
+      <th><?php echo TABLE_HEADING_LAST_MODIFIED; ?></th>
+      <th width="150"><?php echo TABLE_HEADING_ACTION; ?></th>
+      <th align="center" width="20"><?php echo osc_draw_checkbox_field('batchFlag', null, null, 'onclick="flagCheckboxes(this);"'); ?></th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th align="right" colspan="4"><?php echo '<input type="image" src="' . osc_icon_raw('trash.png') . '" title="' . IMAGE_DELETE . '" onclick="document.batch.action=\'' . osc_href_link_admin(FILENAME_DEFAULT, $osC_Template->getModule() . '&action=batchDelete') . '\';" />'; ?></th>
+      <th align="center" width="20"><?php echo osc_draw_checkbox_field('batchFlag', null, null, 'onclick="flagCheckboxes(this);"'); ?></th>
+    </tr>
+  </tfoot>
+  <tbody>
+
+<?php
+  foreach( $cached_files as $cache => $stats ) {
+?>
+
+    <tr onmouseover="rowOverEffect(this);" onmouseout="rowOutEffect(this);">
+      <td onclick="document.getElementById('batch<?php echo $cache; ?>').checked = !document.getElementById('batch<?php echo $cache; ?>').checked;"><?php echo $cache; ?></td>
+      <td align="center"><?php echo $stats['total']; ?></td>
+      <td align="right"><?php echo osC_DateTime::getShort(osC_DateTime::fromUnixTimestamp($stats['last_modified']), true); ?></td>
+      <td align="right"><?php echo osc_link_object(osc_href_link(FILENAME_DEFAULT, $osC_Template->getModule() . '&block=' . $cache . '&action=delete'), osc_icon('trash.png', IMAGE_DELETE)); ?></td>
+      <td align="center"><?php echo osc_draw_checkbox_field('batch[]', $cache, null, 'id="batch' . $cache . '"'); ?></td>
+    </tr>
+
+<?php
+  }
+?>
+
+  </tbody>
+</table>
+
+</form>
+
+<table border="0" width="100%" cellspacing="0" cellpadding="2">
+  <tr>
+    <td style="opacity: 0.5; filter: alpha(opacity=50);"><?php echo '<b>' . TEXT_LEGEND . '</b> ' . osc_icon('trash.png', IMAGE_DELETE) . '&nbsp;' . IMAGE_DELETE; ?></td>
+  </tr>
+</table>
+
+<p><?php echo TEXT_CACHE_DIRECTORY . ' ' . DIR_FS_WORK; ?></p>
