811a0484fdb688bc78b9487b30fa8a71ba015443
Harald Ponce de Leon <hpdl@oscommerce.com>
2007-03-05 22:13:23 +0000

    merge from hpdl r1240
    store the keys in a private array

--- admin/includes/classes/object_info.php
+++ admin/includes/classes/object_info.php
@@ -5,24 +5,30 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2006 osCommerce
+  Copyright (c) 2007 osCommerce
 
   Released under the GNU General Public License
 */
 
   class osC_ObjectInfo {
+    var $_keys = array();
+
     function osC_ObjectInfo($array) {
       foreach ($array as $key => $value) {
-        $this->$key = $value;
+        $this->_keys[$key] = $value;
       }
     }
 
-    function get($value) {
-      return $this->$value;
+    function get($key) {
+      return $this->_keys[$key];
     }
 
+    function getAll() {
+      return $this->_keys;
+    }
+      
     function set($key, $value) {
-      $this->$key = $value;
+      $this->_keys[$key] = $value;
     }
   }
 ?>
