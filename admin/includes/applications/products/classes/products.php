45dddd56ae0e1bce290eef6f3b8344dcec3eddbb
Harald Ponce de Leon <hpdl@oscommerce.com>
2009-03-13 15:17:00 +0100

    symbolize if products have variants or not in the products summary module

--- admin/includes/applications/products/classes/products.php
+++ admin/includes/applications/products/classes/products.php
@@ -23,6 +23,48 @@
       $Qproducts->bindInt(':language_id', $osC_Language->getID());
       $Qproducts->execute();
 
+      $data = $Qproducts->toArray();
+
+      $variants_array = array();
+
+      if ( $data['has_children'] == '1' ) {
+        $Qsubproducts = $osC_Database->query('select * from :table_products where parent_id = :parent_id and products_status = :products_status');
+        $Qsubproducts->bindTable(':table_products', TABLE_PRODUCTS);
+        $Qsubproducts->bindInt(':parent_id', $data['products_id']);
+        $Qsubproducts->bindInt(':products_status', 1);
+        $Qsubproducts->execute();
+
+        while ( $Qsubproducts->next() ) {
+          $variants_array[$Qsubproducts->valueInt('products_id')]['data'] = array('price' => $Qsubproducts->value('products_price'),
+                                                                                  'tax_class_id' => $Qsubproducts->valueInt('products_tax_class_id'),
+                                                                                  'model' => $Qsubproducts->value('products_model'),
+                                                                                  'quantity' => $Qsubproducts->value('products_quantity'),
+                                                                                  'weight' => $Qsubproducts->value('products_weight'),
+                                                                                  'weight_class_id' => $Qsubproducts->valueInt('products_weight_class'),
+                                                                                  'availability_shipping' => 1);
+
+          $Qvariants = $osC_Database->query('select pv.default_combo, pvg.id as group_id, pvg.title as group_title, pvg.module, pvv.id as value_id, pvv.title as value_title, pvv.sort_order as value_sort_order from :table_products_variants pv, :table_products_variants_groups pvg, :table_products_variants_values pvv where pv.products_id = :products_id and pv.products_variants_values_id = pvv.id and pvv.languages_id = :languages_id and pvv.products_variants_groups_id = pvg.id and pvg.languages_id = :languages_id order by pvg.sort_order, pvg.title');
+          $Qvariants->bindTable(':table_products_variants', TABLE_PRODUCTS_VARIANTS);
+          $Qvariants->bindTable(':table_products_variants_groups', TABLE_PRODUCTS_VARIANTS_GROUPS);
+          $Qvariants->bindTable(':table_products_variants_values', TABLE_PRODUCTS_VARIANTS_VALUES);
+          $Qvariants->bindInt(':products_id', $Qsubproducts->valueInt('products_id'));
+          $Qvariants->bindInt(':languages_id', $osC_Language->getID());
+          $Qvariants->bindInt(':languages_id', $osC_Language->getID());
+          $Qvariants->execute();
+
+          while ( $Qvariants->next() ) {
+            $variants_array[$Qsubproducts->valueInt('products_id')]['values'][$Qvariants->valueInt('group_id')][$Qvariants->valueInt('value_id')] = array('value_id' => $Qvariants->valueInt('value_id'),
+                                                                                                                                                          'group_title' => $Qvariants->value('group_title'),
+                                                                                                                                                          'value_title' => $Qvariants->value('value_title'),
+                                                                                                                                                          'sort_order' => $Qvariants->value('value_sort_order'),
+                                                                                                                                                          'default' => (bool)$Qvariants->valueInt('default_combo'),
+                                                                                                                                                          'module' => $Qvariants->value('module'));
+          }
+        }
+      }
+
+      $data['variants'] = $variants_array;
+
       $Qattributes = $osC_Database->query('select id, value from :table_product_attributes where products_id = :products_id and languages_id in (0, :languages_id)');
       $Qattributes->bindTable(':table_product_attributes');
       $Qattributes->bindInt(':products_id', $id);
@@ -35,7 +77,6 @@
         $attributes_array[$Qattributes->valueInt('id')] = $Qattributes->value('value');
       }
 
-      $data = $Qproducts->toArray();
       $data['attributes'] = $attributes_array;
 
       return $data;
--- admin/includes/modules/summary/products.php
+++ admin/includes/modules/summary/products.php
@@ -5,12 +5,14 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2007 osCommerce
+  Copyright (c) 2009 osCommerce
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License v2 (1991)
   as published by the Free Software Foundation.
 */
+
+  require('includes/applications/products/classes/products.php');
 
   if ( !class_exists('osC_Summary') ) {
     include('includes/classes/summary.php');
@@ -57,18 +59,36 @@
                      '  </thead>' .
                      '  <tbody>';
 
-      $Qproducts = $osC_Database->query('select p.products_id, pd.products_name, p.products_price, greatest(p.products_date_added, p.products_last_modified) as date_last_modified, p.products_status from :table_products p, :table_products_description pd where p.products_id = pd.products_id and pd.language_id = :language_id order by date_last_modified desc, pd.products_name limit 6');
+      $Qproducts = $osC_Database->query('select products_id, greatest(products_date_added, products_last_modified) as date_last_modified from :table_products where parent_id = 0 order by date_last_modified desc limit 6');
       $Qproducts->bindTable(':table_products', TABLE_PRODUCTS);
-      $Qproducts->bindTable(':table_products_description', TABLE_PRODUCTS_DESCRIPTION);
-      $Qproducts->bindInt(':language_id', $osC_Language->getID());
       $Qproducts->execute();
 
       while ( $Qproducts->next() ) {
+        $data = osC_Products_Admin::get($Qproducts->valueInt('products_id'));
+
+        $products_icon = osc_icon('products.png');
+        $products_price = $data['products_price'];
+
+        if ( !empty($data['variants']) ) {
+          $products_icon = osc_icon('attach.png');
+          $products_price = null;
+
+          foreach ( $data['variants'] as $variant ) {
+            if ( ($products_price === null) || ($variant['data']['price'] < $products_price) ) {
+              $products_price = $variant['data']['price'];
+            }
+          }
+
+          if ( $products_price === null ) {
+            $products_price = 0;
+          }
+        }
+
         $this->_data .= '    <tr onmouseover="rowOverEffect(this);" onmouseout="rowOutEffect(this);">' .
-                        '      <td>' . osc_link_object(osc_href_link_admin(FILENAME_DEFAULT, 'products&pID=' . $Qproducts->valueInt('products_id') . '&action=save'), osc_icon('products.png') . '&nbsp;' . $Qproducts->value('products_name')) . '</td>' .
-                        '      <td>' . $osC_Currencies->format($Qproducts->value('products_price')) . '</td>' .
+                        '      <td>' . osc_link_object(osc_href_link_admin(FILENAME_DEFAULT, 'products=' . (int)$data['products_id'] . '&action=save'), $products_icon . '&nbsp;' . osc_output_string_protected($data['products_name'])) . '</td>' .
+                        '      <td>' . ( !empty($data['variants']) ? 'from ' : '' ) . $osC_Currencies->format($products_price) . '</td>' .
                         '      <td>' . $Qproducts->value('date_last_modified') . '</td>' .
-                        '      <td align="center">' . osc_icon(($Qproducts->valueInt('products_status') === 1) ? 'checkbox_ticked.gif' : 'checkbox_crossed.gif', null, null) . '</td>' .
+                        '      <td align="center">' . osc_icon(((int)$data['products_status'] === 1) ? 'checkbox_ticked.gif' : 'checkbox_crossed.gif', null, null) . '</td>' .
                         '    </tr>';
       }
 
