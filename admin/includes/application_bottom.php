072449cc4fa2337ca5207eb199d93ac7a06161f2
Harald Ponce de Leon <hazza@oscommerce.com>
2009-03-27 01:49:35 +0800

    Let PHP close the session automatically via the register_shutdown_function call from the session class constructor
    Signed-off-by: Harald Ponce de Leon <hpdl@oscommerce.com>

--- admin/includes/application_bottom.php
+++ admin/includes/application_bottom.php
@@ -5,13 +5,10 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2004 osCommerce
+  Copyright (c) 2009 osCommerce
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License v2 (1991)
   as published by the Free Software Foundation.
 */
-
-// close session
-  $osC_Session->close();
 ?>
