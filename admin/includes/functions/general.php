0aca865e5539dee42dd960b9643998bf4febb107
Harald Ponce de Leon <hpdl@oscommerce.com>
2009-11-26 13:27:05 +0100

    Don't close the session prematurely as it is already automatically being taken care of in the register_shutdown_function scope.

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -1,11 +1,7 @@
 <?php
 /*
-  $Id$
-
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2009 osCommerce
+  osCommerce Online Merchant $osCommerce-SIG$
+  Copyright (c) 2009 osCommerce (http://www.oscommerce.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License v2 (1991)
@@ -44,8 +40,6 @@
     }
 
     header('Location: ' . $url);
-
-    $osC_Session->close();
 
     exit;
   }
