f3df03e82d2ad93adaf4a55f2702e1f2e60102ca
Gergely <tgely@gumisarok.hu>
2010-07-29 03:22:10 +0800

    Modify onmouseover in some place

--- osCommerce/OM/Core/Site/Admin/Application/ZoneGroups/pages/entries.php
+++ osCommerce/OM/Core/Site/Admin/Application/ZoneGroups/pages/entries.php
@@ -105,7 +105,7 @@
       var newRow = $('#' + dataTableName)[0].tBodies[0].insertRow(rowCounter);
       newRow.id = 'row' + parseInt(record.association_id);
 
-      $('#row' + parseInt(record.association_id)).hover( function() { rowOverEffect(this); }, function() { rowOutEffect(this); }).click(function(event) {
+      $('#row' + parseInt(record.association_id)).hover( function() { $(this).addClass('mouseOver'); }, function() { $(this).removeClass('mouseOver'); }).click(function(event) {
         if (event.target.type !== 'checkbox') {
           $(':checkbox', this).trigger('click');
         }
