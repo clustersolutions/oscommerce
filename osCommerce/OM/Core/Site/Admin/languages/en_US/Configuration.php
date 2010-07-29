4d89af72f2f2280bc95163f9147f7ec263ff9327
Gergely <tgely@gumisarok.hu>
2010-07-29 17:57:31 +0800

    Add Admin Configuration Batch Edit Language variables

--- osCommerce/OM/Core/Site/Admin/languages/en_US/Configuration.php
+++ osCommerce/OM/Core/Site/Admin/languages/en_US/Configuration.php
@@ -15,3 +15,6 @@ table_heading_action = Action
 table_heading_groups = Groups
 
 introduction_edit_parameter = Please make the necessary changes for this configuration parameter.
+
+action_heading_batch_edit_configuration_parameters = Batch Edit Configuration
+introduction_batch_edit_configuration_parameters = Please make the necessary changes for the following configurations.
