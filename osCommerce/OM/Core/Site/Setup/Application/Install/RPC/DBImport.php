92ce7ef8a1466b439edb6751cb7a5f336653c584
Harald Ponce de Leon <hpdl@oscommerce.com>
2010-08-20 16:27:23 +0800

    Modularize RPC (Setup Site)

--- osCommerce/OM/Core/Site/Setup/Application/Install/RPC/DBImport.php
+++ osCommerce/OM/Core/Site/Setup/Application/Install/RPC/DBImport.php
@@ -8,40 +8,15 @@
   as published by the Free Software Foundation.
 */
 
-  namespace osCommerce\OM\Core\Site\Setup\Application\Install;
+  namespace osCommerce\OM\Core\Site\Setup\Application\Install\RPC;
 
   use osCommerce\OM\Core\Registry;
   use osCommerce\OM\Core\Database;
   use osCommerce\OM\Core\OSCOM;
   use osCommerce\OM\Core\DirectoryListing;
 
-  class RPC {
-    public static function dbCheck() {
-      $db = array('DB_SERVER' => trim(urldecode($_POST['server'])),
-                  'DB_SERVER_USERNAME' => trim(urldecode($_POST['username'])),
-                  'DB_SERVER_PASSWORD' => trim(urldecode($_POST['password'])),
-                  'DB_DATABASE' => trim(urldecode($_POST['name'])),
-                  'DB_SERVER_PORT' => trim(urldecode($_POST['port'])),
-                  'DB_DATABASE_CLASS' => trim(urldecode(str_replace('_', '\\', $_POST['class'])))
-                 );
-
-      Registry::set('Database', Database::initialize($db['DB_SERVER'], $db['DB_SERVER_USERNAME'], $db['DB_SERVER_PASSWORD'], $db['DB_DATABASE'], $db['DB_SERVER_PORT'], $db['DB_DATABASE_CLASS']));
-      Registry::set('osC_Database', Registry::get('Database')); // HPDL to delete
-
-      $OSCOM_Database = Registry::get('Database');
-
-      if ( !$OSCOM_Database->isError() ) {
-        $OSCOM_Database->selectDatabase($db['DB_DATABASE']);
-      }
-
-      if ( !$OSCOM_Database->isError() ) {
-        echo '[[1]]';
-      } else {
-        echo '[[0|' . $OSCOM_Database->getError() . ']]';
-      }
-    }
-
-    public static function dbImport() {
+  class DBImport {
+    public static function execute() {
       $db = array('DB_SERVER' => trim(urldecode($_POST['server'])),
                   'DB_SERVER_USERNAME' => trim(urldecode($_POST['username'])),
                   'DB_SERVER_PASSWORD' => trim(urldecode($_POST['password'])),
@@ -53,8 +28,6 @@
                  );
 
       Registry::set('Database', Database::initialize($db['DB_SERVER'], $db['DB_SERVER_USERNAME'], $db['DB_SERVER_PASSWORD'], $db['DB_DATABASE'], $db['DB_SERVER_PORT'], $db['DB_DATABASE_CLASS']));
-      Registry::set('osC_Database', Registry::get('Database')); // HPDL to delete
-
       $OSCOM_Database = Registry::get('Database');
 
       if ( !$OSCOM_Database->isError() ) {
@@ -204,34 +177,6 @@
 
           $OSCOM_Database->importSQL($innodb_sql_file, $db['DB_DATABASE'], $db['DB_TABLE_PREFIX']);
         }
-      }
-
-      if ( !$OSCOM_Database->isError() ) {
-        echo '[[1]]';
-      } else {
-        echo '[[0|' . $OSCOM_Database->getError() . ']]';
-      }
-    }
-
-    public static function dbImportSample() {
-      $db = array('DB_SERVER' => trim(urldecode($_POST['server'])),
-                  'DB_SERVER_USERNAME' => trim(urldecode($_POST['username'])),
-                  'DB_SERVER_PASSWORD' => trim(urldecode($_POST['password'])),
-                  'DB_DATABASE' => trim(urldecode($_POST['name'])),
-                  'DB_SERVER_PORT' => trim(urldecode($_POST['port'])),
-                  'DB_DATABASE_CLASS' => trim(urldecode(str_replace('_', '\\', $_POST['class']))),
-                  'DB_TABLE_PREFIX' => trim(urldecode($_POST['prefix']))
-                 );
-
-      Registry::set('Database', Database::initialize($db['DB_SERVER'], $db['DB_SERVER_USERNAME'], $db['DB_SERVER_PASSWORD'], $db['DB_DATABASE'], $db['DB_SERVER_PORT'], $db['DB_DATABASE_CLASS']));
-      Registry::set('osC_Database', Registry::get('Database')); // HPDL to delete
-
-      $OSCOM_Database = Registry::get('Database');
-
-      if ( !$OSCOM_Database->isError() ) {
-        $sql_file = OSCOM::BASE_DIRECTORY . 'Core/Site/Setup/sql/oscommerce_sample_data.sql';
-
-        $OSCOM_Database->importSQL($sql_file, $db['DB_DATABASE'], $db['DB_TABLE_PREFIX']);
       }
 
       if ( !$OSCOM_Database->isError() ) {
