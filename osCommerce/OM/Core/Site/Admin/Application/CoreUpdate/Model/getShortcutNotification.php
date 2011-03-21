2d7ff0e4d84d307d395011c4f4217b2d4df98f60
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-03-21 19:56:10 +0800

    Add CoreUpdate shortcut notification

---
+++ osCommerce/OM/Core/Site/Admin/Application/CoreUpdate/Model/getShortcutNotification.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  osCommerce Online Merchant $osCommerce-SIG$
+  Copyright (c) 2010 osCommerce (http://www.oscommerce.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+*/
+
+  namespace osCommerce\OM\Core\Site\Admin\Application\CoreUpdate\Model;
+
+  use osCommerce\OM\Core\Site\Admin\Application\CoreUpdate\CoreUpdate;
+
+  class getShortcutNotification {
+    public static function execute($datetime) {
+      $result = CoreUpdate::getAvailablePackages();
+
+      return $result['total'];
+    }
+  }
+?>
