65f7b939b1fc3657e867d849d339d7855d3fd8ef
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-03-28 16:57:23 +0200

    Fix typo; import MySQL V5 CountriesGetAll stored procedure during MySQL v5 installation

--- osCommerce/OM/Core/Site/Setup/Application/Install/SQL/MySQL/V5/ImportFK.php
+++ osCommerce/OM/Core/Site/Setup/Application/Install/SQL/MySQL/V5/ImportFK.php
@@ -19,7 +19,32 @@
 
       $sql_file = OSCOM::BASE_DIRECTORY . 'Core/Site/Setup/sql/oscommerce_innodb.sql';
 
-      return $OSCOM_PDO->importSQL($sql_file, $data['table_prefix']);
+      $OSCOM_PDO->importSQL($sql_file, $data['table_prefix']);
+
+      $OSCOM_PDO->exec('DROP PROCEDURE IF EXISTS CountriesGetAll;
+CREATE PROCEDURE CountriesGetAll (IN pageset INT, IN maxresults INT)
+BEGIN
+  IF pageset is null THEN
+    SELECT SQL_CALC_FOUND_ROWS c.*, COUNT(z.zone_id) AS total_zones
+    FROM osc_countries c
+    LEFT JOIN osc_zones z ON (c.countries_id = z.zone_country_id)
+    GROUP BY c.countries_id
+    ORDER BY c.countries_name;
+  ELSE
+    PREPARE STMT FROM
+      "SELECT SQL_CALC_FOUND_ROWS c.*, COUNT(z.zone_id) AS total_zones
+       FROM osc_countries c
+       LEFT JOIN osc_zones z ON (c.countries_id = z.zone_country_id)
+       GROUP BY c.countries_id
+       ORDER BY c.countries_name
+       LIMIT ?, ?";
+    SET @START = pageset;
+    SET @LIMIT = maxresults;
+    EXECUTE STMT USING @START, @LIMIT;
+  END IF;
+
+  SELECT FOUND_ROWS() as total;
+END;');
     }
   }
 ?>
