c20b21fb287d2151d01ea7dea855f32b0dbcaaf3
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-04-18 17:45:16 +0200

    Force ErrorHandler messages to UTF-8

--- osCommerce/OM/Core/Language.php
+++ osCommerce/OM/Core/Language.php
@@ -236,5 +236,50 @@
 
       return HTML::image(OSCOM::getPublicSiteLink('images/worldflags/' . $image_code . '.png', null, 'Shop'), $this->_languages[$code]['name'], $width, $height, $parameters);
     }
+
+/**
+ * Converts string to UTF-8
+ *
+ * @param string $string The string to convert to UTF-8
+ * @return string
+ * @since v3.0.2
+ */
+
+    public static function toUTF8($string) {
+      if ( !static::isUTF8($string) ) {
+        $string = iconv('CP1252', 'UTF-8', $string);
+      }
+
+      return $string;
+    }
+
+/**
+ * Detect if a string is UTF-8
+ *
+ * @param string $string The string to detect
+ * @return boolean
+ * @since v3.0.2
+ */
+    public static function isUTF8($string) {
+      if ( strlen($string) > 5000 ) {
+        for ( $i=0, $s=5000, $j = ceil(strlen($string)/5000); $i < $j; $i++, $s += 5000 ) {
+          if ( static::isUTF8(substr($string, $s, 5000)) ) {
+            return true;
+          }
+        }
+
+        return false;
+      } else {
+        return preg_match('%^(?:[\x09\x0A\x0D\x20-\x7E]           # ASCII
+                              | [\xC2-\xDF][\x80-\xBF]            # non-overlong 2-byte
+                              |  \xE0[\xA0-\xBF][\x80-\xBF]       # excluding overlongs
+                              | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte
+                              |  \xED[\x80-\x9F][\x80-\xBF]       # excluding surrogates
+                              |  \xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
+                              | [\xF1-\xF3][\x80-\xBF]{3}         # planes 4-15
+                              |  \xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
+                             )*$%xs', $string);
+      }
+    }
   }
 ?>
