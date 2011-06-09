b985232f1804838061dfccbfd41a30f809f63e74
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-06-09 13:42:33 +0200

    Allow configuration parameters to be retrieved by their keys

--- osCommerce/OM/Core/Site/Admin/Application/Configuration/SQL/MySQL/Standard/EntryGet.php
+++ osCommerce/OM/Core/Site/Admin/Application/Configuration/SQL/MySQL/Standard/EntryGet.php
@@ -14,8 +14,14 @@
     public static function execute($data) {
       $OSCOM_PDO = Registry::get('PDO');
 
-      $Qcfg = $OSCOM_PDO->prepare('select * from :table_configuration where configuration_id = :configuration_id');
-      $Qcfg->bindInt(':configuration_id', $data['id']);
+      if ( isset($data['key']) ) {
+        $Qcfg = $OSCOM_PDO->prepare('select * from :table_configuration where configuration_key = :configuration_key');
+        $Qcfg->bindValue(':configuration_key', $data['key']);
+      } else {
+        $Qcfg = $OSCOM_PDO->prepare('select * from :table_configuration where configuration_id = :configuration_id');
+        $Qcfg->bindInt(':configuration_id', $data['id']);
+      }
+
       $Qcfg->execute();
 
       return $Qcfg->fetch();
