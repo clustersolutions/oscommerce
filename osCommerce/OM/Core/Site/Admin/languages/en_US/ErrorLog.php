cd0f837fb70fc4bcf560c3a2812c06bab4dfe3c1
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-06-14 14:13:46 +0200

    Replace Error Log delete page with a modal dialog

--- osCommerce/OM/Core/Site/Admin/languages/en_US/ErrorLog.php
+++ osCommerce/OM/Core/Site/Admin/languages/en_US/ErrorLog.php
@@ -10,6 +10,5 @@ heading_title = Error Log
 table_heading_date = Date
 table_heading_message = Message
 
-title_delete_error_log = Delete Error Log File
-introduction_delete_error_log = Please verify the removal of the error log file.
-number_of_error_log_file_entries = Total error log file entries to delete: %s
+dialog_delete_error_log_title = Delete Error Log
+dialog_delete_error_log_desc = All Error Log entries will be permanently deleted and cannot be recovered. Are you sure?
