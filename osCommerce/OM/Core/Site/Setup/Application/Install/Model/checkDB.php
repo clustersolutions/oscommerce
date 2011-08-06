b01de568ea8757d3304a379d652a59ec3e6117e6
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-08-06 16:27:04 +0200

    Create the database during setup if it does not already exist

--- osCommerce/OM/Core/Site/Setup/Application/Install/Model/checkDB.php
+++ osCommerce/OM/Core/Site/Setup/Application/Install/Model/checkDB.php
@@ -8,10 +8,18 @@
 
   namespace osCommerce\OM\Core\Site\Setup\Application\Install\Model;
 
+  use osCommerce\OM\Core\OSCOM;
   use osCommerce\OM\Core\PDO;
+  use osCommerce\OM\Core\Registry;
 
   class checkDB {
     public static function execute($data) {
+      if ( $OSCOM_PDO = PDO::initialize($data['server'], $data['username'], $data['password'], null, $data['port'], $data['class']) ) {
+        Registry::set('PDO', $OSCOM_PDO);
+
+        OSCOM::callDB('Setup\Install\CreateDB', array('database' => $data['database']));
+      }
+
       return PDO::initialize($data['server'], $data['username'], $data['password'], $data['database'], $data['port'], $data['class']);
     }
   }
