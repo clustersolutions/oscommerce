c6ed9330837dd3ed24f3e959d7429589fc96fe35
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-10-13 15:18:58 +0200

    ANSI/PostgreSQL updates

--- osCommerce/OM/Core/Site/Admin/Application/ProductAttributes/SQL/ANSI/GetAll.php
+++ osCommerce/OM/Core/Site/Admin/Application/ProductAttributes/SQL/ANSI/GetAll.php
@@ -6,7 +6,7 @@
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
-  namespace osCommerce\OM\Core\Site\Admin\Application\ProductAttributes\SQL\MySQL\Standard;
+  namespace osCommerce\OM\Core\Site\Admin\Application\ProductAttributes\SQL\ANSI;
 
   use osCommerce\OM\Core\Registry;
 
