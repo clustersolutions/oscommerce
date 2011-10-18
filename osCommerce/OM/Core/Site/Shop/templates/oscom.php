6322e11873db069be30b4aa8d83a0132a14f0281
Harald Ponce de Leon <hpdl@oscommerce.com>
2011-10-18 19:04:41 +0200

    Update jQuery to 1.6.4 Update jQuery UI to 1.8.16

--- osCommerce/OM/Core/Site/Shop/templates/oscom.php
+++ osCommerce/OM/Core/Site/Shop/templates/oscom.php
@@ -25,10 +25,10 @@
 
 <meta name="generator" value="osCommerce Online Merchant" />
 
-<script type="text/javascript" src="public/external/jquery/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="public/external/jquery/jquery-1.6.4.min.js"></script>
 
-<link rel="stylesheet" type="text/css" href="public/external/jquery/ui/themes/start/jquery-ui-1.8.13.custom.css" />
-<script type="text/javascript" src="public/external/jquery/ui/jquery-ui-1.8.13.custom.min.js"></script>
+<link rel="stylesheet" type="text/css" href="public/external/jquery/ui/themes/start/jquery-ui-1.8.16.custom.css" />
+<script type="text/javascript" src="public/external/jquery/ui/jquery-ui-1.8.16.custom.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="<?php echo OSCOM::getPublicSiteLink('templates/oscom/stylesheets/general.css'); ?>" />
 
