eb71954873afdf1cec50e3686e679596093160c6
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-03-02 20:14:56 +0100

    Add optional parameter to lang tag to parse the string value with a specified function

--- osCommerce/OM/Core/Template/Tag/lang.php
+++ osCommerce/OM/Core/Template/Tag/lang.php
@@ -12,7 +12,13 @@
 
   class lang extends \osCommerce\OM\Core\Template\TagAbstract {
     static public function execute($string) {
-      return OSCOM::getDef($string);
+      $args = func_get_args();
+
+      if ( isset($args[1]) && !empty($args[1]) ) {
+        return call_user_func(trim($args[1]), OSCOM::getDef($string));
+      } else {
+        return OSCOM::getDef($string);
+      }
     }
   }
 ?>
