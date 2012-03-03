cfb42d1fb28a85f3ecc34c271e495a64aa6149cc
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-03-03 11:56:44 +0100

    Move application value to module

--- osCommerce/OM/Core/Site/Admin/Application/Login/Controller.php
+++ osCommerce/OM/Core/Site/Admin/Application/Login/Controller.php
@@ -9,7 +9,6 @@
   namespace osCommerce\OM\Core\Site\Admin\Application\Login;
 
   use osCommerce\OM\Core\OSCOM;
-  use osCommerce\OM\Core\Registry;
 
   class Controller extends \osCommerce\OM\Core\Site\Admin\ApplicationAbstract {
     protected $_link_to = false;
@@ -19,13 +18,7 @@
     protected function initialize() {}
 
     protected function process() {
-      $OSCOM_Template = Registry::get('Template');
-
       $this->_page_title = OSCOM::getDef('heading_title');
-
-      $OSCOM_Template->setValue('page_icon', $OSCOM_Template->getIcon(32, $this->_icon));
-      $OSCOM_Template->setValue('page_title', $this->_page_title);
-      $OSCOM_Template->setValue('lang_field_show_password', addslashes(OSCOM::getDef('field_show_password')));
     }
   }
 ?>
---
+++ osCommerce/OM/Core/Site/Admin/Application/Login/Module/Template/Value/show_password/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Admin\Application\Login\Module\Template\Value\show_password;
+
+  class Controller extends \osCommerce\OM\Core\Template\ValueAbstract {
+    static public function execute() {
+      return false;
+    }
+  }
+?>
