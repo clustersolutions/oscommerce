fad72a737b04be2250731bd5c5712e34462da9cc
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-10 01:55:54 +0200

    Shortcut Notifications RPC updates
    * Add rpcStatus to the result
    * Group entries in the result
    
    Conflicts:
    	osCommerce/OM/Core/Site/Admin/templates/oscom/header.php

--- osCommerce/OM/Core/Site/Admin/Application/Dashboard/RPC/GetShortcutNotifications.php
+++ osCommerce/OM/Core/Site/Admin/Application/Dashboard/RPC/GetShortcutNotifications.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
   namespace osCommerce\OM\Core\Site\Admin\Application\Dashboard\RPC;
 
-  use osCommerce\OM\Core\Site\Admin\Application\Dashboard\Dashboard;
   use osCommerce\OM\Core\OSCOM;
+  use osCommerce\OM\Core\Site\Admin\Application\Dashboard\Dashboard;
+  use osCommerce\OM\Core\Site\RPC\Controller as RPC;
 
   class GetShortcutNotifications {
     public static function execute() {
       $site = OSCOM::getSite();
 
-      $result = array();
+      $result = array('entries' => array());
 
       if ( isset($_SESSION[$site]['id']) ) {
         if ( isset($_GET['reset']) && !empty($_GET['reset']) && OSCOM::siteApplicationExists($_GET['reset']) ) {
@@ -31,11 +32,13 @@
         foreach ( $_SESSION[$site]['access'] as $module => $data ) {
           if ( $data['shortcut'] === true ) {
             if ( method_exists('osCommerce\\OM\\Core\\Site\\Admin\\Application\\' . $data['module'] . '\\' . $data['module'], 'getShortcutNotification') || class_exists('osCommerce\\OM\\Core\\Site\\Admin\\Application\\' . $data['module'] . '\\Model\\getShortcutNotification') ) {
-              $result[$data['module']] = call_user_func(array('osCommerce\\OM\\Core\\Site\\Admin\\Application\\' . $data['module'] . '\\' . $data['module'], 'getShortcutNotification'), $shortcuts[$data['module']]);
+              $result['entries'][$data['module']] = call_user_func(array('osCommerce\\OM\\Core\\Site\\Admin\\Application\\' . $data['module'] . '\\' . $data['module'], 'getShortcutNotification'), $shortcuts[$data['module']]);
             }
           }
         }
       }
+
+      $result['rpcStatus'] = RPC::STATUS_SUCCESS;
 
       echo json_encode($result);
     }
