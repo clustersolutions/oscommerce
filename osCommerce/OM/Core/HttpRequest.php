f1ab89a009a3d88e09dea8bde27d076b97ceb708
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-10 02:09:27 +0200

    RPC updates
    * Set HTTP 403 on failed requests
    * Catch redirects during Site and Application initialization and return STATUS_REDIRECT_DETECTED status
    * Add setResponseCode($code) to HttpRequest

--- osCommerce/OM/Core/HttpRequest.php
+++ osCommerce/OM/Core/HttpRequest.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
@@ -14,7 +14,7 @@
 /**
  *
  * @param array $parameters url, header, parameters, method, certificate
- * @param string $driver 
+ * @param string $driver
  */
 
     public static function getResponse($parameters, $driver = null) {
@@ -55,6 +55,84 @@
       }
 
       return trim(call_user_func(array('osCommerce\\OM\\Core\\HttpRequest\\' . $driver, 'execute'), $parameters));
+    }
+
+/**
+ * Set the HTTP status code
+ *
+ * @param int $code The HTTP status code to set
+ * @return boolean
+ * @since v3.0.3
+ */
+
+    public static function setResponseCode($code) {
+      if ( !is_numeric($code) ) {
+        trigger_error('HttpRequest::setResponseCode() - $code value is not numeric.', E_USER_ERROR);
+
+        return false;
+      }
+
+      if ( headers_sent() ) {
+        trigger_error('HttpRequest::setResponseCode() - headers already sent, cannot set response code.', E_USER_ERROR);
+
+        return false;
+      }
+
+      if ( function_exists('http_response_code') ) { /* HPDL PHP 5.4.0 */
+        http_response_code($code);
+
+        return true;
+      } else {
+        $codes = array('100' => 'Continue',
+                       '101' => 'Switching Protocols',
+                       '200' => 'OK',
+                       '201' => 'Created',
+                       '202' => 'Accepted',
+                       '203' => 'Non-Authoritative Information',
+                       '204' => 'No Content',
+                       '205' => 'Reset Content',
+                       '206' => 'Partial Content',
+                       '300' => 'Multiple Choices',
+                       '301' => 'Moved Permanently',
+                       '302' => 'Moved Temporarily',
+                       '303' => 'See Other',
+                       '304' => 'Not Modified',
+                       '305' => 'Use Proxy',
+                       '400' => 'Bad Request',
+                       '401' => 'Unauthorized',
+                       '402' => 'Payment Required',
+                       '403' => 'Forbidden',
+                       '404' => 'Not Found',
+                       '405' => 'Method Not Allowed',
+                       '406' => 'Not Acceptable',
+                       '407' => 'Proxy Authentication Required',
+                       '408' => 'Request Time-out',
+                       '409' => 'Conflict',
+                       '410' => 'Gone',
+                       '411' => 'Length Required',
+                       '412' => 'Precondition Failed',
+                       '413' => 'Request Entity Too Large',
+                       '414' => 'Request-URI Too Large',
+                       '415' => 'Unsupported Media Type',
+                       '500' => 'Internal Server Error',
+                       '501' => 'Not Implemented',
+                       '502' => 'Bad Gateway',
+                       '503' => 'Service Unavailable',
+                       '504' => 'Gateway Time-out',
+                       '505' => 'HTTP Version not supported');
+
+        if ( isset($codes[$code]) ) {
+          $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+          header($protocol . ' ' . $code . ' ' . $codes[$code]);
+
+          return true;
+        } else {
+          trigger_error('HttpRequest::setResponseCode() - Unknown status code \'' . $code . '\'.', E_USER_ERROR);
+        }
+      }
+
+      return false;
     }
   }
 ?>
--- osCommerce/OM/Core/Site/RPC/Controller.php
+++ osCommerce/OM/Core/Site/RPC/Controller.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
   namespace osCommerce\OM\Core\Site\RPC;
 
-  use osCommerce\OM\Core\OSCOM;
   use osCommerce\OM\Core\HTML;
+  use osCommerce\OM\Core\HttpRequest;
+  use osCommerce\OM\Core\OSCOM;
 
   class Controller implements \osCommerce\OM\Core\SiteInterface {
     const STATUS_SUCCESS = 1;
-    const STATUS_NO_SESSION = -10;
+    const STATUS_REDIRECT_DETECTED = -10;
     const STATUS_NO_MODULE = -20;
     const STATUS_NO_ACCESS = -50;
     const STATUS_CLASS_NONEXISTENT = -60;
@@ -27,61 +28,77 @@
       header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
       header('Content-Type: application/json; charset=utf-8');
 
-      if ( empty($_GET) ) {
-        echo json_encode(array('rpcStatus' => self::STATUS_NO_MODULE));
-        exit;
-      }
-
-      $site = HTML::sanitize(basename(key(array_slice($_GET, 1, 1, true))));
-      $application = HTML::sanitize(basename(key(array_slice($_GET, 2, 1,  true))));
-
-      if ( !OSCOM::siteExists($site) ) {
-        echo json_encode(array('rpcStatus' => self::STATUS_CLASS_NONEXISTENT));
-        exit;
-      }
-
-      OSCOM::setSite($site);
-
-      if ( !OSCOM::siteApplicationExists($application) ) {
-        echo json_encode(array('rpcStatus' => self::STATUS_CLASS_NONEXISTENT));
-        exit;
-      }
-
-      OSCOM::setSiteApplication($application);
-
-      call_user_func(array('osCommerce\\OM\\Core\\Site\\' . $site . '\\Controller', 'initialize'));
-
-      if ( !call_user_func(array('osCommerce\\OM\\Core\\Site\\' . $site . '\\Controller', 'hasAccess'), $application)) {
-        echo json_encode(array('rpcStatus' => self::STATUS_NO_ACCESS));
-        exit;
-      }
-
-      if ( count($_GET) < 3 ) {
-        echo json_encode(array('rpcStatus' => self::STATUS_NO_ACTION));
-        exit;
-      }
-
-      $rpc_called = false;
-
-      $rpc = array('RPC');
-
-      for ( $i = 3, $n = count($_GET); $i < $n; $i++ ) {
-        $subrpc = HTML::sanitize(basename(key(array_slice($_GET, $i, 1, true))));
-
-        if ( self::siteApplicationRPCExists(implode('\\', $rpc) . '\\' . $subrpc) ) {
-          call_user_func(array('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\' . implode('\\', $rpc) . '\\' . $subrpc, 'execute'));
-
-          $rpc[] = $subrpc;
-
-          $rpc_called = true;
-        } else {
-          break;
+      try {
+        if ( empty($_GET) ) {
+          throw new \Exception(self::STATUS_NO_MODULE);
+        } elseif ( count($_GET) < 3 ) {
+          throw new \Exception(self::STATUS_NO_ACTION);
         }
-      }
 
-      if ( $rpc_called === false ) {
-        echo json_encode(array('rpcStatus' => self::STATUS_NO_ACTION));
-        exit;
+        $site = HTML::sanitize(basename(key(array_slice($_GET, 1, 1, true))));
+        $application = HTML::sanitize(basename(key(array_slice($_GET, 2, 1,  true))));
+
+        if ( !OSCOM::siteExists($site) ) {
+          throw new \Exception(self::STATUS_CLASS_NONEXISTENT);
+        }
+
+        OSCOM::setSite($site);
+
+        if ( !OSCOM::siteApplicationExists($application) ) {
+          throw new \Exception(self::STATUS_CLASS_NONEXISTENT);
+        }
+
+        OSCOM::setSiteApplication($application);
+
+        ob_start( function($buffer) {
+          foreach ( headers_list() as $h ) {
+            if ( stripos($h, 'Location:') !== false ) {
+              header_remove('Location');
+
+              \osCommerce\OM\Core\HttpRequest::setResponseCode(403);
+
+              $buffer = json_encode(array('rpcStatus' => constant('osCommerce\\OM\\Core\\Site\\RPC\\Controller::STATUS_REDIRECT_DETECTED')));
+
+              break;
+            }
+          }
+
+          return $buffer;
+        });
+
+        call_user_func(array('osCommerce\\OM\\Core\\Site\\' . $site . '\\Controller', 'initialize'));
+
+        ob_end_flush();
+
+        if ( !call_user_func(array('osCommerce\\OM\\Core\\Site\\' . $site . '\\Controller', 'hasAccess'), $application)) {
+          throw new \Exception(self::STATUS_NO_ACCESS);
+        }
+
+        $rpc_called = false;
+
+        $rpc = array('RPC');
+
+        for ( $i = 3, $n = count($_GET); $i < $n; $i++ ) {
+          $subrpc = HTML::sanitize(basename(key(array_slice($_GET, $i, 1, true))));
+
+          if ( self::siteApplicationRPCExists(implode('\\', $rpc) . '\\' . $subrpc) ) {
+            call_user_func(array('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\' . implode('\\', $rpc) . '\\' . $subrpc, 'execute'));
+
+            $rpc[] = $subrpc;
+
+            $rpc_called = true;
+          } else {
+            break;
+          }
+        }
+
+        if ( $rpc_called === false ) {
+          throw new \Exception(self::STATUS_NO_ACTION);
+        }
+      } catch ( \Exception $e ) {
+        HttpRequest::setResponseCode(403);
+
+        echo json_encode(array('rpcStatus' => $e->getMessage()));
       }
 
       exit;
