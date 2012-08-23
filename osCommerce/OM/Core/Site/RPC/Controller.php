4be12f242ea42c2b21680826b181cdf073682d8a
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-08-24 00:37:20 +0200

    Improve cache headers

--- osCommerce/OM/Core/Site/RPC/Controller.php
+++ osCommerce/OM/Core/Site/RPC/Controller.php
@@ -24,9 +24,12 @@
     protected static $_default_application = 'Index';
 
     public static function initialize() {
-      header('Cache-Control: no-cache, must-revalidate');
-      header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
       header('Content-Type: application/json; charset=utf-8');
+      header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+      header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+      header('Cache-Control: no-store, no-cache, must-revalidate');
+      header('Cache-Control: post-check=0, pre-check=0', false);
+      header('Pragma: no-cache');
 
       try {
         if ( empty($_GET) ) {
