a232d9c6b2e122d478267949ff2d6b5daa3b2fd8
Harald Ponce de Leon <hpdl@oscommerce.com>
2014-02-14 15:25:35 +0100

    improve getRandomString() internals

--- osCommerce/OM/Core/Hash.php
+++ osCommerce/OM/Core/Hash.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
+ *
+ * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
 
@@ -46,26 +46,90 @@
     }
 
     public static function getRandomString($length, $type = 'mixed') {
-      if ( !in_array($type, array('mixed', 'chars', 'digits')) ) {
+      if ( !in_array($type, [ 'mixed', 'chars', 'digits' ]) ) {
+        trigger_error('Hash::getRandomString() $type not recognized:' . $type, E_USER_ERROR);
+
         return false;
       }
 
-      $chars_pattern = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-      $mixed_pattern = '1234567890' . $chars_pattern;
+      $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $digits = '0123456789';
+
+      $base = '';
+
+      if ( ($type == 'mixed') || ($type == 'chars') ) {
+        $base .= $chars;
+      }
+
+      if ( ($type == 'mixed') || ($type == 'digits') ) {
+        $base .= $digits;
+      }
 
       $rand_value = '';
 
-      while ( strlen($rand_value) < $length ) {
-        if ( $type == 'digits' ) {
-          $rand_value .= rand(0, 9);
-        } elseif ( $type == 'chars' ) {
-          $rand_value .= substr($chars_pattern, rand(0, 51), 1);
-        } else {
-          $rand_value .= substr($mixed_pattern, rand(0, 61), 1);
+      do {
+        $random = base64_encode(static::getRandomBytes($length));
+
+        for ($i=0, $n=strlen($random); $i<$n; $i++) {
+          $char = substr($random, $i, 1);
+
+          if ( strpos($base, $char) !== false ) {
+            $rand_value .= $char;
+          }
         }
+      } while ( strlen($rand_value) < $length );
+
+      if ( strlen($rand_value) > $length ) {
+        $rand_value = substr($rand_value, 0, $length);
       }
 
       return $rand_value;
+    }
+
+	public static function getRandomBytes($length) {
+      static $random_state;
+
+      if ( !isset($random_state) ) {
+        $random_state = microtime();
+
+        if ( function_exists('getmypid') ) {
+          $random_state .= getmypid();
+        }
+      }
+
+      $result = '';
+
+      if ( @is_readable('/dev/urandom') && ($fh = @fopen('/dev/urandom', 'rb')) ) {
+        if ( function_exists('stream_set_read_buffer') ) {
+          stream_set_read_buffer($fh, 0);
+        }
+
+        $result = fread($fh, $length);
+
+        fclose($fh);
+      } elseif ( function_exists('openssl_random_pseudo_bytes') ) {
+        $result = openssl_random_pseudo_bytes($length, $orpb_secure);
+
+        if ( $orpb_secure != true ) {
+          $result = '';
+        }
+      } elseif ( defined('MCRYPT_DEV_URANDOM') ) {
+        $result = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+      }
+
+      if ( strlen($result) < $length) {
+        $result = '';
+
+        for ( $i=0; $i<$length; $i+=16 ) {
+          $random_state = md5(microtime() . $random_state);
+
+          $result .= pack('H*', md5($random_state));
+        }
+
+        $result = substr($result, 0, $length);
+      }
+
+      return $result;
     }
   }
 ?>
