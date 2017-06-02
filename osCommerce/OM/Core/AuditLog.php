a99c9064edc2131b2a2905b2bb91e9292e20e05b
Harald Ponce de Leon <hpdl@oscommerce.com>
2017-06-02 16:03:28 +0200

    add recursive array diff method

--- osCommerce/OM/Core/AuditLog.php
+++ osCommerce/OM/Core/AuditLog.php
@@ -2,45 +2,68 @@
 /**
  * osCommerce Online Merchant
  *
- * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
- * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ * @copyright (c) 2017 osCommerce; https://www.oscommerce.com
+ * @license BSD; https://www.oscommerce.com/bsdlicense.txt
  */
 
-  namespace osCommerce\OM\Core;
+namespace osCommerce\OM\Core;
 
-  use osCommerce\OM\Core\OSCOM;
+use osCommerce\OM\Core\OSCOM;
 
-/**
- * @since v3.0.3
- */
+class AuditLog
+{
+    public static function save($data)
+    {
+        if (!isset($data['site'])) {
+            $data['site'] = OSCOM::getSite();
+        }
 
-  class AuditLog {
-    public static function save($data) {
-      if ( !isset($data['site']) ) {
-        $data['site'] = OSCOM::getSite();
-      }
+        if (!isset($data['application'])) {
+            $data['application'] = OSCOM::getSiteApplication();
+        }
 
-      if ( !isset($data['application']) ) {
-        $data['application'] = OSCOM::getSiteApplication();
-      }
+        if (!isset($data['action'])) {
+            $data['action'] = null;
+        }
 
-      if ( !isset($data['action']) ) {
-        $data['action'] = null;
-      }
-
-      OSCOM::callDB('SaveAuditLog', $data, 'Core');
+        OSCOM::callDB('SaveAuditLog', $data, 'Core');
     }
 
-    public static function getAll($req, $id, $limit = 10) {
-      $sig = explode('\\', $req, 3);
+    public static function getAll($req, $id, $limit = 10)
+    {
+        $sig = explode('\\', $req, 3);
 
-      $data = [ 'site' => $sig[0],
-                'application' => $sig[1],
-                'action' => $sig[2],
-                'id' => $id,
-                'limit' => $limit ];
+        $data = [
+            'site' => $sig[0],
+            'application' => $sig[1],
+            'action' => $sig[2],
+            'id' => $id,
+            'limit' => $limit
+        ];
 
-      return OSCOM::callDB('GetAuditLog', $data, 'Core');
+        return OSCOM::callDB('GetAuditLog', $data, 'Core');
     }
-  }
-?>
+
+    public static function getDiff(array $array1, array $array2)
+    {
+        $difference = [];
+
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (!array_key_exists($key, $array2) || !is_array($array2[$key])) {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = static::getDiff($value, $array2[$key]);
+
+                    if (!empty($new_diff)) {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            } else if (!array_key_exists($key, $array2) || ($array2[$key] !== $value)) {
+                $difference[$key] = $value;
+            }
+        }
+
+        return $difference;
+    }
+}
