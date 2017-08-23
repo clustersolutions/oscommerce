519617698570a84e670cfea1d7959212610d0bb6
Harald Ponce de Leon <hpdl@oscommerce.com>
2017-08-23 18:06:17 +0200

    add support for parameters to be passed

--- osCommerce/OM/Core/Template/Tag/publiclink.php
+++ osCommerce/OM/Core/Template/Tag/publiclink.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * osCommerce Online Merchant
- * 
+ *
  * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
  * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
  */
@@ -14,13 +14,17 @@
     static protected $_parse_result = false;
 
     static public function execute($string) {
-      $params = explode('|', $string, 2);
+      $params = explode('|', $string, 3);
 
-      if ( !isset($params[1]) ) {
+      if ( !isset($params[1]) || empty($params[1]) ) {
         $params[1] = null;
       }
 
-      return OSCOM::getPublicSiteLink($params[0], null, $params[1]);
+      if ( !isset($params[2]) || empty($params[2]) ) {
+        $params[2] = null;
+      }
+
+      return OSCOM::getPublicSiteLink($params[0], $params[2], $params[1]);
     }
   }
 ?>
